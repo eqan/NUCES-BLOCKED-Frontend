c82506d508b07bcc6c01fc67b03af64ba630edd2
nomZ1 <nomi92760@gmail.com>
2023-01-09 02:32:52 +0500

    Download page

---
+++ pages/landing/index.js
@@ -0,0 +1,75 @@
+import React, { useContext, useRef } from 'react';
+import Link from 'next/link';
+import getConfig from 'next/config';
+import { StyleClass } from 'primereact/styleclass';
+import { Button } from 'primereact/button';
+import { Ripple } from 'primereact/ripple';
+import { Divider } from 'primereact/divider';
+import AppConfig from '../../layout/AppConfig';
+import { LayoutContext } from '../../layout/context/layoutcontext';
+import { InputText } from 'primereact/inputtext';
+
+const LandingPage = () => {
+    const contextPath = getConfig().publicRuntimeConfig.contextPath;
+    const { layoutConfig } = useContext(LayoutContext);
+    const menuRef = useRef();
+
+    return (
+        <div className="surface-0 flex justify-content-center">
+            <div id="home" className="landing-wrapper overflow-hidden">
+                
+
+                <div
+                    id="hero"
+                    className="flex flex-column pt-4 px-4 lg:px-8 overflow-hidden"
+                    style={{ background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, #FFFFFF 5%, #515354 100%)', clipPath: 'ellipse(150% 87% at 93% 13%)' ,width:1730}}>
+                    <div className="mx-4 md:mx-8 mt-0 md:mt-4">
+                        <span className="text-6xl font-bold text-gray-900 line-height-2" >
+                            <h1 style={{fontSize: 120, color:'##D7FCFF',margin:168,textAlign:'center'}}>NUCES BLOCKED</h1>
+                        </span>
+                    <div  style={{position:'absolute',left: 200,top: 520}}>
+                    <div >
+                            
+                            <InputText inputid="email1" type="text" placeholder="Enter Hash" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
+
+                          </div>
+                        <div >
+                            
+                        <Button type="button" label="Download" className="p-button-rounded text-xl border-none mt-3 bg-blue-1000 font-normal line-height-3 px-3 text-white"></Button>
+                       
+                          </div>
+                    </div> 
+                    
+                    
+                    </div>    
+                       
+                    <div className="flex justify-content-center md:justify-content-end">
+                        <img src={`${contextPath}/demo/images/landing/logo.png`} alt="Hero Image" className="w-9 md:w-auto" />
+                    </div>
+                   
+                </div>
+
+                <div>
+                            
+                        </div>
+
+                
+
+                
+
+                
+            </div>
+        </div>
+    );
+};
+
+LandingPage.getLayout = function getLayout(page) {
+    return (
+        <React.Fragment>
+            {page}
+            <AppConfig simple />
+        </React.Fragment>
+    );
+};
+
+export default LandingPage;
