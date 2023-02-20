7e67c35e41e090934818ae2989daee3b36fa8a4b
eqan <eqan.ahmad123@gmail.com>
2023-02-20 19:46:15 +0500

    10-crud-operations-implementation-in-all-pages: Fixing bug in authentication

--- layout/context/requireAuthetication.tsx
+++ layout/context/requireAuthetication.tsx
@@ -10,26 +10,50 @@ query   ($userEmail: String!) {
 
 export function requireAuthentication(gssp: GetServerSideProps){
     return async (ctx: GetServerSidePropsContext)=>{
-        const {req}=ctx;
-        if(req.headers.cookie){
-            const tokens=req.headers.cookie.split(';');
-            const token=tokens.find((token)=>token.includes('access_token'));
-            const userEmail=((jwt.decode((tokens[1].split('='))[1].toString())).email.toString());
-            apolloClient.query({
-                query:GET_USER_TYPE,
-                variables:{userEmail},
-            }).then((result)=>{
-                console.log(result.data.GetUserTypeByUserEmail);
-            });
-            if(!token){
-                return{
-                    redirect:{
-                        permanent:false,
-                        destination:'/auth/login',
-                    },
-                };
+        try{
+            const {req}=ctx;
+            if(req.headers.cookie){
+                const tokens=req.headers.cookie.split(';');
+                const token=tokens.find((token)=>token.includes('access_token'));
+                let userEmail = "";
+                if (tokens) {
+                    userEmail=((jwt.decode((tokens[1].split('='))[1].toString())).email.toString());
+                    }
+                apolloClient.query({
+                    query:GET_USER_TYPE,
+                    variables: {userEmail},
+                }).then((result)=>{
+                    console.log(result.data.GetUserTypeByUserEmail);
+                });
+                if(!token){
+                    return{
+                        redirect:{
+                            permanent:false,
+                            destination:'/auth/login',
+                        },
+                    };
+                }
             }
+            else
+            {
+                return {
+                  redirect: {
+                    permanent: false,
+                    destination: '/auth/login',
+                  },
+               };
+            }
+            return await gssp(ctx);
         }
-        return await gssp(ctx);
+        catch(error)
+        {
+            console.log(error);
+            return {
+              redirect: {
+                permanent: false,
+                destination: '/auth/login',
+              },
+           };
+        }
     };
 }
