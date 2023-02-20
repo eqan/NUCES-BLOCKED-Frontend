ca75b6763be00a2dbc1f3eb8f6fbbaf48549e68d
eqan <eqan.ahmad123@gmail.com>
2023-02-20 19:51:06 +0500

    10-crud-operations-implementation-in-all-pages: Optimizing code

--- layout/context/requireAuthetication.tsx
+++ layout/context/requireAuthetication.tsx
@@ -1,59 +1,57 @@
-import { GetServerSideProps, GetServerSidePropsContext} from 'next';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { gql } from '@apollo/client';
 import apolloClient from '../../apollo-client';
 import jwt from 'jsonwebtoken';
 
 const GET_USER_TYPE = gql`
-query   ($userEmail: String!) {
-    GetUserTypeByUserEmail (userEmail: $userEmail)
-}`;
+  query GetUserTypeByUserEmail($userEmail: String!) {
+    GetUserTypeByUserEmail(userEmail: $userEmail)
+  }
+`;
 
-export function requireAuthentication(gssp: GetServerSideProps){
-    return async (ctx: GetServerSidePropsContext)=>{
-        try{
-            const {req}=ctx;
-            if(req.headers.cookie){
-                const tokens=req.headers.cookie.split(';');
-                const token=tokens.find((token)=>token.includes('access_token'));
-                let userEmail = "";
-                if (tokens) {
-                    userEmail=((jwt.decode((tokens[1].split('='))[1].toString())).email.toString());
-                    }
-                apolloClient.query({
-                    query:GET_USER_TYPE,
-                    variables: {userEmail},
-                }).then((result)=>{
-                    console.log(result.data.GetUserTypeByUserEmail);
-                });
-                if(!token){
-                    return{
-                        redirect:{
-                            permanent:false,
-                            destination:'/auth/login',
-                        },
-                    };
-                }
-            }
-            else
-            {
-                return {
-                  redirect: {
-                    permanent: false,
-                    destination: '/auth/login',
-                  },
-               };
-            }
-            return await gssp(ctx);
-        }
-        catch(error)
-        {
-            console.log(error);
-            return {
-              redirect: {
-                permanent: false,
-                destination: '/auth/login',
-              },
-           };
-        }
+export const requireAuthentication = (gssp: GetServerSideProps) => async (
+  ctx: GetServerSidePropsContext
+) => {
+  try {
+    const { req } = ctx;
+    const token = req.headers.cookie?.split(';').find((cookie) => cookie.includes('access_token'));
+
+    if (!token) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/auth/login',
+        },
+      };
+    }
+
+    const userEmail = jwt.decode(token.split('=')[1])?.email?.toString();
+
+    if (!userEmail) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/auth/login',
+        },
+      };
+    }
+
+    const { data } = await apolloClient.query({
+      query: GET_USER_TYPE,
+      variables: { userEmail },
+    });
+
+    console.log(data.GetUserTypeByUserEmail);
+
+    return gssp(ctx);
+  } catch (error) {
+    console.log(error);
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/auth/login',
+      },
     };
-}
+  }
+};
