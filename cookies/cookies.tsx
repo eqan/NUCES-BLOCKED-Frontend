6fb6de66020bfa13cb1ade0fc652f64cbab47b01
eqan <eqan.ahmad123@gmail.com>
2023-04-05 11:16:15 +0500

    Main: Cleaning unnecessary files

--- cookies/cookies.tsx
+++
@@ -1,8 +0,0 @@
-import Cookies from 'js-cookie';
-
-export function getCookie(): string | null {
-    const cookies = Cookies.get('access_token');
-    console.log(cookies);
-    //const access_token=cookies.find((token)=>token.includes('access_token'));
-    return null;//access_token.toString();
-  }
