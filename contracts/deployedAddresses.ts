d002d7eb6c5ba50a3c08aa7582846d53ea6c2abb
eqan <eqan.ahmad123@gmail.com>
2023-04-19 03:20:24 +0500

    Main: Updating certificate contract

--- contracts/deployedAddresses.ts
+++ contracts/deployedAddresses.ts
@@ -1,4 +1,4 @@
 export enum DeployedContracts {
     SemesterStore = '0x9d90e940066828e239Ba4D24CA5e9534aA20970F',
-    CertificateStore = '0xE77183C28cFC407aC7113F9884D84b67Cdbad715',
+    CertificateStore = '0x6cB32a157e6c3Cb6aB5F901f8194704B0f88f061',
 }
