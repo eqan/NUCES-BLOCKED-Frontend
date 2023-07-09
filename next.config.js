e01ece029740f517c71b96472725893cd7fcb536
eqan <eqan.ahmad123@gmail.com>
2023-07-09 14:35:41 +0500

    77-proposal-integeration: Finally the deployment bugs are fixed 'yarn run build' is working fine now

--- next.config.js
+++ next.config.js
@@ -4,9 +4,8 @@ const nextFonts = require('next-fonts');
 module.exports = nextFonts({
   reactStrictMode: true,
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/nuces-blocked' : '',
+  basePath: '',
   publicRuntimeConfig: {
     contextPath: process.env.NODE_ENV === 'production' ? '/nuces-blocked' : '',
-    uploadPath: process.env.NODE_ENV === 'production' ? '/nuces-blocked/upload.php' : '/api/upload',
   },
 });
